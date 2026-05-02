c0a90ac7d5a8d3014ffcf411715b7399c48a68ac
Rajeev Sharma <talk2rajeev1986@gmail.com>
2026-05-02 15:52:10 +0530

    Fix root redirect for Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://keshavdhamseva.org",
+  site: "https://keshavdhamseva.netlify.app",
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "en",
